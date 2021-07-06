64a5fa9eb0f81eda96650857e91f8bbf3dc35a03
james-99k <jamesgasty@gmail.com>
2021-07-06 21:33:05 +0800

    back end

---
+++ src/main/java/com/example/CRUD/controller/TodoController.java
@@ -0,0 +1,112 @@
+package com.example.CRUD.controller;
+
+
+import com.example.CRUD.model.Todo;
+import com.example.CRUD.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class TodoController {
+
+    @Autowired
+    TodoRepository todoRepository;
+
+    @GetMapping("/todos")
+    public ResponseEntity<List<Todo>> getAllTutorials(@RequestParam(required = false) String title) {
+        try {
+            List<Todo> todos = new ArrayList<>();
+
+            if (title == null)
+                todoRepository.findAll().forEach(todos::add);
+            else
+                todoRepository.findByTitleContaining(title).forEach(todos::add);
+            if (todos.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(todos, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable("id") long id) {
+        Optional<Todo> todoData = todoRepository.findById(id);
+
+        if (todoData.isPresent()) {
+            return new ResponseEntity<>(todoData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/todos")
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
+        try {
+            Todo _todo = todoRepository
+                    .save(new Todo(todo.getTitle(), todo.getDescription(), false));
+            return new ResponseEntity<>(_todo, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable("id") long id, @RequestBody Todo todo) {
+        Optional<Todo> todoData = todoRepository.findById(id);
+
+        if (todoData.isPresent()) {
+            Todo _todo = todoData.get();
+            _todo.setTitle(todo.getTitle());
+            _todo.setDescription(todo.getDescription());
+            _todo.setPublished(todo.isPublished());
+            return new ResponseEntity<>(todoRepository.save(_todo), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public ResponseEntity<HttpStatus> deleteTodo(@PathVariable("id") long id) {
+        try {
+            todoRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/todos")
+    public ResponseEntity<HttpStatus> deleteAllTodo() {
+        try {
+            todoRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/todos/published")
+    public ResponseEntity<List<Todo>> findByPublished() {
+        try {
+            List<Todo> todos = todoRepository.findByPublished(true);
+
+            if (todos.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(todos, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
