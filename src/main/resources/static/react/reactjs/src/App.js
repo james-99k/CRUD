cb34b518ceb6f984493d884113468a425551ca48
james-99k <jamesgasty@gmail.com>
2021-07-13 18:11:16 +0800

    CRUD

--- src/main/resources/static/react/reactjs/src/App.js
+++ src/main/resources/static/react/reactjs/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
+import AddTodo from '';
+import Todo from '';
+import TodoList from '';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+        <Router>
+            <div>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <a href="/todos" className="navbar-brand">
+                    Todo
+               </a>
+               <div className="navbar-nav mr-auto">
+
+               </div>
+            </div>
+        <Router>
+    )
+  }
 }
 
 export default App;
